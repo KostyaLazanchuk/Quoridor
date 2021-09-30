6479d783413ac5c5f856dea50d9ade46a182cabe
Stavolo <54746320+Stavolo@users.noreply.github.com>
2021-09-30 20:18:39 +0300

    Move ver.0.1

--- Quoridor/MoveChar/Move.cs
+++ Quoridor/MoveChar/Move.cs
@@ -6,6 +6,6 @@ namespace Quoridor.MoveChar
 {
     class Move
     {
-        int a;
+        
     }
 }
--- Quoridor/Program.cs
+++ Quoridor/Program.cs
@@ -7,10 +7,21 @@ namespace Quoridor
     {
         static void Main(string[] args)
         {
-            int[,] map = new int[17, 17];
+            int[,] map = new int[19, 19];
             DrawMap drawMap = new DrawMap();
             map = drawMap.Map();
-           
+            int costx = 17, y = 1,x = 9;
+            while (true)
+            {
+                Console.CursorLeft = costx;
+                Console.CursorTop = y;
+                Console.Write("#");
+                ConsoleKeyInfo ki = Console.ReadKey(true);
+                if (ki.Key == ConsoleKey.LeftArrow && costx - 4 >= 0 && map[y,x-1] == 2) { x = x - 2; costx = costx - 4; }
+                if (ki.Key == ConsoleKey.RightArrow && costx + 4 <= 34 && map[y, x+1] == 2) { x = x + 2; costx = costx + 4; }
+                if (ki.Key == ConsoleKey.UpArrow && y-2 >= 0 && map[y - 1, x] == 2) y=y-2;
+                if (ki.Key == ConsoleKey.DownArrow && y+2 <= 17 && map[y + 1, x] == 2) y=y+2;
+            }
         }
     }
 }
