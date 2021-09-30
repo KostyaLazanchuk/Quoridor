a233f265fe6e89f974dba09bb1d28b009c8d9cca
Stavolo <stasnislav4@gmail.com>
2021-09-30 18:33:34 +0300

    Change map again...

--- Quoridor/Program.cs
+++ Quoridor/Program.cs
@@ -10,14 +10,7 @@ namespace Quoridor
             int[,] map = new int[17, 17];
             DrawMap drawMap = new DrawMap();
             map = drawMap.Map();
-            for (int i = 0; i < 17; i++)
-            {
-                for (int j = 0; j < 17; j++)
-                {
-                    Console.Write(map[i, j]);
-                }
-                Console.WriteLine();
-            }
+           
         }
     }
 }
