24de4fb061f23a4b260945059bef92c4023696cf
Kostya Lazanchuk <90553568+KostyaLazanchuk@users.noreply.github.com>
2021-10-03 20:33:29 +0300

    Move to master brach menu folder

---
+++ Quoridor/Menu/Play.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quoridor.Menu
+{
+    class Play : ResultPage
+    {
+        public void StartGamesVsAI()
+        {
+            //вызов метода с игрой
+            Console.WriteLine("на стадии разработки");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Victory(ref a);
+        }
+        public void StartGamesVsPlayer()
+        {
+            //вызов метода с игрой
+            Console.WriteLine("на стадии разработки");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Victory(ref a);
+
+        }
+    }
+}
