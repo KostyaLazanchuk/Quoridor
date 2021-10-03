cd06c1bb63cf514d6d548b3e1872511003276ccf
vasRiddla <65939154+vasRiddla@users.noreply.github.com>
2021-10-03 20:10:05 +0300

    AI base

--- Quoridor/AI/AI.cs
+++ Quoridor/AI/AI.cs
@@ -6,6 +6,40 @@ namespace Quoridor.AI
 {
     class AI
     {
+        public void Choose()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 2);
+            if (number == 1)
+            {
+                //Ходить
+                int move = random.Next(1, 4);
+                switch (move)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+            }
+            else
+            {
+                //Ставить стенки
+                int position = random.Next(1, 2);
+                switch (position)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }
 
+
+            }
+        }
     }
 }
