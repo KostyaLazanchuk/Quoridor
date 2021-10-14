05caf179cc2a5c879e6676a481da6759482a09b8
Stavolo <54746320+Stavolo@users.noreply.github.com>
2021-10-14 19:52:00 +0300

    Change moveWall

--- Quoridor/MoveChar/Move.cs
+++ Quoridor/MoveChar/Move.cs
@@ -10,7 +10,7 @@ namespace Quoridor.MoveChar
     {
         /*public int costx1 = 17, playerY1 = 1, playerX1 = 9;*/
 
-        public void MoveP(ref int costx, ref int playerY, ref int playerX, ref int name, ref int[,] map)
+        public void MoveP(ref int costx, ref int playerY, ref int playerX, ref int name, ref int[,] map, ref int contsP)
         {
             /*int[,] map = new int[19, 19];
             DrawMap drawMap = new DrawMap();
@@ -32,6 +32,7 @@ namespace Quoridor.MoveChar
                 playerX = playerX - 2;
                 costx = costx - 4;
                 map[playerY, playerX] = name;
+                contsP++;
                 
             }
             if (player1.Key == ConsoleKey.RightArrow && costx + 4 <= 34 && map[playerY, playerX + 1] == 2)
@@ -39,14 +40,14 @@ namespace Quoridor.MoveChar
                 map[playerY, playerX] = 0;
                 playerX = playerX + 2; costx = costx + 4;
                 map[playerY, playerX] = name;
-                
+                contsP++;
             }
             if (player1.Key == ConsoleKey.UpArrow && playerY - 2 >= 0 && map[playerY - 1, playerX] == 2)
             {
                 map[playerY, playerX] = 0;
                 playerY = playerY - 2;
                 map[playerY, playerX] = name;
-                
+                contsP++;
             }
 
             if (player1.Key == ConsoleKey.DownArrow && playerY + 2 <= 17 && map[playerY + 1, playerX] == 2)
@@ -54,7 +55,7 @@ namespace Quoridor.MoveChar
                 map[playerY, playerX] = 0;
                 playerY = playerY + 2;
                 map[playerY, playerX] = name;
-                
+                contsP++;
             }
 
 
--- Quoridor/Process.cs
+++ Quoridor/Process.cs
@@ -20,22 +20,9 @@ namespace Quoridor
             int costx1 = 17, y1 = 17, x1 = 9;
             int name1 = 5;
             int name2 = 4;
-            int conts = 0;
+            int contsW = 0;
+            int contsP = 0;
 
-            /*while (true)
-            {
-                if (conts == 0)
-                {
-                    Console.Clear();
-                    drawMap.Paint(map);
-                    movew.MoveW(ref map, ref conts);
-                }
-                else
-                {
-                    drawMap.Paint(map);
-                    conts--;
-                }
-            }*/
             while (true)
             {
                 drawMap.Paint(map);
@@ -48,31 +35,45 @@ namespace Quoridor
                     switch (step)
                     {
                         case "1":
-                            Thread.Sleep(500);
-                            move.MoveP(ref costx, ref y, ref x, ref name1, ref map);
-                            part++;
+                            while (true)
+                            {
+                                if (contsP == 0)
+                                {
+                                    Console.Clear();
+                                    drawMap.Paint(map);
+                                    move.MoveP(ref costx, ref y, ref x, ref name1, ref map, ref contsP);
+                                }
+                                else
+                                {
+                                    part++;
+                                    contsP--;
+                                    break;
+                                }
+                            }
                             Console.Clear();
                             break;
                         case "2":
                             while (true)
                             {
-                                if (conts == 0)
+                                if (contsW == 0)
                                 {
                                     Console.Clear();
                                     drawMap.Paint(map);
-                                    movew.MoveW(ref map, ref conts);
+                                    movew.MoveW(ref map, ref contsW);
                                 }
                                 else
                                 {
                                     drawMap.Paint(map);
-                                    conts--;
+                                    contsW--;
+                                    part++;
+                                    break;
                                 }
 
                             }
                             Console.Clear();
                             break;
                         default:
-                            Console.WriteLine("Не прввильно сделан ход (намжмите любую кнопку, чтобы продолжить)");
+                            Console.WriteLine("Не правильно сделан ход (намжмите любую кнопку, чтобы продолжить)");
                             Console.ReadKey();
                             Console.Clear();
                             break;
@@ -85,31 +86,45 @@ namespace Quoridor
                     switch (step)
                     {
                         case "1":
-                            Thread.Sleep(500);
-                            move.MoveP(ref costx1, ref y1, ref x1, ref name2, ref map);
-                            part--;
+                            while (true)
+                            {
+                                if(contsP == 0)
+                                {
+                                    Console.Clear();
+                                    drawMap.Paint(map);
+                                    move.MoveP(ref costx1, ref y1, ref x1, ref name2, ref map, ref contsP);
+                                }
+                                else 
+                                {
+                                    part--;
+                                    contsP--;
+                                    break; 
+                                }
+                            }
                             Console.Clear();
                             break;
                         case "2":
                             while (true)
                             {
-                                if (conts == 0)
+                                if (contsW == 0)
                                 {
                                     Console.Clear();
                                     drawMap.Paint(map);
-                                    movew.MoveW(ref map, ref conts);
+                                    movew.MoveW(ref map, ref contsW);
                                 }
                                 else
                                 {
                                     drawMap.Paint(map);
-                                    conts--;
+                                    contsW--;
+                                    part--;
+                                    break;
                                 }
 
                             }
                             Console.Clear();
                             break;
                         default:
-                            Console.WriteLine("Не прввильно сделан ход (намжмите любую кнопку, чтобы продолжить)");
+                            Console.WriteLine("Не правильно сделан ход (намжмите любую кнопку, чтобы продолжить)");
                             Console.ReadKey();
                             Console.Clear();
                             break;
@@ -117,17 +132,6 @@ namespace Quoridor
 
                     }
                 }
-            }
-
-            while (true)
-            {
-                Console.SetCursorPosition(costx, y);
-                Console.Write("#");
-                ConsoleKeyInfo ki = Console.ReadKey(true);
-                if (ki.Key == ConsoleKey.LeftArrow && costx - 4 >= 0 && map[y, x - 1] == 2) { x = x - 2; costx = costx - 4; }
-                if (ki.Key == ConsoleKey.RightArrow && costx + 4 <= 34 && map[y, x + 1] == 2) { x = x + 2; costx = costx + 4; }
-                if (ki.Key == ConsoleKey.UpArrow && y - 2 >= 0 && map[y - 1, x] == 2) y = y - 2;
-                if (ki.Key == ConsoleKey.DownArrow && y + 2 <= 17 && map[y + 1, x] == 2) y = y + 2;
             }
         }
     }
